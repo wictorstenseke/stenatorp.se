6c9cd26a32db513fb4fa792d5d33d1b987541664
wictorstenseke <wictor.johansson@gmail.com>
2016-05-30 13:16:34 +0200

    added a dropdown navbar

--- includes/nav.php
+++ includes/nav.php
@@ -18,12 +18,26 @@
       <a href="../index.php"><img src="../img/logostenatorp.svg" alt="Stenatorps logotype"></a>
     </div>
 
-    <nav>
+    <div class="menu-toggle">
+      <img src="../img/menu-toggle.svg" alt="Knapp för att öppna menyn">
+    </div>
+
+    <nav class="medium-nav">
       <a href="../sjalvplock.php">Självplock</a>
       <a href="../forsaljning.php">Försäljning</a>
       <a href="#">Kontakt / Hitta hit</a>
     </nav>
 
+  </div>
+
+  <div class="mobil-nav">
+      <div class="hr-small-nav"></div>
+      <li><a href="../sjalvplock.php">Självplock</a></li>
+      <div class="hr-small-nav"></div>
+      <li><a href="../forsaljning.php">Försäljning</a></li>
+      <div class="hr-small-nav"></div>
+      <li><a href="#">Kontakt / Hitta hit</a></li>
+      <div class="hr-small-nav"></div>
   </div>
 
 </header>

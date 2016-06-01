98e54783eea0446cc36be17e527bb283169eedd8
wictorstenseke <wictor.johansson@gmail.com>
2016-06-01 10:18:31 +0200

    moved footer

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 
     <?php include "includes/pages/include-index.php" ?>
 
-  </div>
+    <?php include "includes/footer.php" ?>
 
-  <?php include "includes/footer.php" ?>
+  </div>
 
   <script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
   <script type="text/javascript" src="js/min/functions-min.js"></script>
--- sjalvplock.php
+++ sjalvplock.php
@@ -12,9 +12,9 @@
 
     <?php include "includes/pages/include-sjalvplock.php" ?>
 
-  </div>
+    <?php include "includes/footer.php" ?>
 
-  <?php include "includes/footer.php" ?>
+  </div>
 
   <script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
   <script type="text/javascript" src="js/min/functions-min.js"></script>

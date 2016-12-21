5999214dcd0d99311ee9e463e08251358f38c8f0
Wictor Stenseke <wictorstenseke@Wictors-MBP.lan>
2016-12-22 00:50:02 +0100

    minor committsion

--- includes/nav.php
+++ includes/nav.php
@@ -20,15 +20,15 @@
   </div>
 
   <div class="mobil-nav">
-      <div class="hr-small-nav"></div>
-      <a href="forsaljning.php"><li class="<?php echo ($page == "forsaljning" ? "active" : "")?>">Försäljning</li></a>
-      <div class="hr-small-nav"></div>
-      <a href="sjalvplock.php"><li class="<?php echo ($page == "sjalvplock" ? "active" : "")?>">Självplock</li></a>
-      <div class="hr-small-nav"></div>
-      <a href="kontakt.php"><li class="<?php echo ($page == "kontakt" ? "active" : "")?>">Kontakt / Hitta hit</li></a>
-      <div class="hr-small-nav"></div>
-      <a href="recept.php"><li class="<?php echo ($page == "recept" ? "active" : "")?>">Recept</li></a>
-      <div class="hr-small-nav-last"></div>
+    <div class="hr-small-nav"></div>
+    <a href="forsaljning.php"><li class="<?php echo ($page == "forsaljning" ? "active" : "")?>">Försäljning</li></a>
+    <div class="hr-small-nav"></div>
+    <a href="sjalvplock.php"><li class="<?php echo ($page == "sjalvplock" ? "active" : "")?>">Självplock</li></a>
+    <div class="hr-small-nav"></div>
+    <a href="kontakt.php"><li class="<?php echo ($page == "kontakt" ? "active" : "")?>">Kontakt / Hitta hit</li></a>
+    <div class="hr-small-nav"></div>
+    <a href="recept.php"><li class="<?php echo ($page == "recept" ? "active" : "")?>">Recept</li></a>
+    <div class="hr-small-nav-last"></div>
   </div>
 
 </header>
